a6e8b6827804c9df00d5441a32a1b927dd04dee4
ramiz773 <aleemrameestpn@gmail.com>
2024-09-21 11:24:58 +0530

    change color pellet

--- frontend/src/component/Navbar.js
+++ frontend/src/component/Navbar.js
@@ -4,33 +4,33 @@ import useLogout from "../Hooks/useLogout";
 import useAuthContext from "../Hooks/useAuthContext";
 
 const Navbar = () => {
-   const { user } = useAuthContext();
-   const logout = useLogout();
-   const handleLogout = () => {
-      logout();
-   };
-   return (
-      <header>
-         <div className="container">
-            <Link to={"/"}>
-               <h1>Workout Buddy</h1>
-            </Link>
-            <nav>
-               {user ? (
-                  <div>
-                     <span>{user.email}</span>
-                     <button onClick={handleLogout}>Log out</button>
-                  </div>
-               ) : (
-                  <div>
-                     <Link to={"/login"}>Login</Link>
-                     <Link to={"/signup"}>Signup</Link>
-                  </div>
-               )}
-            </nav>
-         </div>
-      </header>
-   );
+  const { user } = useAuthContext();
+  const logout = useLogout();
+  const handleLogout = () => {
+    logout();
+  };
+  return (
+    <header>
+      <div className="container">
+        <Link to={"/"}>
+          <h1>workout app</h1>
+        </Link>
+        <nav>
+          {user ? (
+            <div>
+              <span>{user.email}</span>
+              <button onClick={handleLogout}>Log out</button>
+            </div>
+          ) : (
+            <div>
+              <Link to={"/login"}>Login</Link>
+              <Link to={"/signup"}>Signup</Link>
+            </div>
+          )}
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
--- frontend/src/component/WorkoutDetails.js
+++ frontend/src/component/WorkoutDetails.js
@@ -5,43 +5,43 @@ import { RiDeleteBin6Line } from "react-icons/ri";
 import useAuthContext from "../Hooks/useAuthContext";
 
 const WorkoutDetails = ({ workout }) => {
-   const { user } = useAuthContext();
-   const { dispatch } = useWorkoutContext();
-   const handleDelete = async () => {
-      if (!user) {
-         return;
-      }
-      const res = await fetch("/api/workouts/" + workout._id, {
-         method: "DELETE",
-         headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${user.token}`,
-         },
-      });
-      const json = await res.json();
-      console.log(json);
-      if (res.ok) {
-         dispatch({ type: "DELETE_WORKOUT", payload: json });
-      }
-   };
-   return (
-      <div className="workout" key={workout._id}>
-         <h4>{workout.title}</h4>
-         <p>
-            <strong>Load (kg): </strong> {workout.title}
-         </p>
-         <p>
-            <strong>Reps: </strong> {workout.reps}
-         </p>
-         <p>
-            <strong>Load: </strong> {workout.title}
-         </p>
-         <p>{moment(workout.createdAt).fromNow()}</p>
-         <span onClick={handleDelete}>
-            <RiDeleteBin6Line size={20} />
-         </span>
-      </div>
-   );
+  const { user } = useAuthContext();
+  const { dispatch } = useWorkoutContext();
+  const handleDelete = async () => {
+    if (!user) {
+      return;
+    }
+    const res = await fetch("/api/workouts/" + workout._id, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${user.token}`,
+      },
+    });
+    const json = await res.json();
+    console.log(json);
+    if (res.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
+    }
+  };
+  return (
+    <div className="workout" key={workout._id}>
+      <h4>{workout.title}</h4>
+      <p>
+        <strong>Load (kg): </strong> {workout.title}
+      </p>
+      <p>
+        <strong>Reps: </strong> {workout.reps}
+      </p>
+      <p>
+        <strong>Load: </strong> {workout.title}
+      </p>
+      <p>{moment(workout.createdAt).fromNow()}</p>
+      <span onClick={handleDelete} className="dlt-workout">
+        <RiDeleteBin6Line size={20} />
+      </span>
+    </div>
+  );
 };
 
 export default WorkoutDetails;
